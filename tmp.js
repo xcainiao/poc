965f4e4db7024075b23b88c72aecbb902e0c4d45
xcainiao <1581300646@qq.com>
2019-06-03 22:44:55 +0800

    njs poc
    njs poc

---
+++ tmp.js
@@ -0,0 +1,14 @@
+ate.pirvMatvMath.PIe= h.PIe= 
+
+
+
+
+
+
+Ib;;;;;;;;{ -
+
+
+
+
+
+/{  *le ((v0.getUTCMonth(String(), (Boolean), (JSOvMatMath.PIe= h.Path.PIe= hyIe= th.PIe=[[[[[[_[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[S[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[ hN).Iÿÿÿÿÿ,ÿÿ 
